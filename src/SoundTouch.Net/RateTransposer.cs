6a30052d076af5f0a8432b368f5560e79499f6a3
Olaf Woudenberg <o.woudenberg@g2speech.com>
2021-07-01 23:25:22 +0200

    Call "clear()" after changing anti-alias filter on/off Call "clear()" after changing anti-alias filter on/off to prefill buffers appropriately.

--- src/SoundTouch.Net/RateTransposer.cs
+++ src/SoundTouch.Net/RateTransposer.cs
@@ -109,6 +109,7 @@ namespace SoundTouch
 #if !SOUNDTOUCH_PREVENT_CLICK_AT_RATE_CROSSOVER
             // Disable Anti-alias filter if desirable to avoid click at rate change zero value crossover
             _useAAFilter = newMode;
+            Clear();
 #endif
         }
 
